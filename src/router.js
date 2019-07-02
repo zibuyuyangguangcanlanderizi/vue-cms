13925ec9dd5b6d0138b8dcfdf06ccaf57f36a807
ljf <ljf@qq.com>
2019-07-02 22:36:51 +0800

    项目第一天

--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 import VueRouter from 'vue-router'
 
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    {path:'/', redirect: '/home'},
+    {path:'/home', component: HomeContainer},
+    {path:'/member', component: MemberContainer},
+    {path:'/shopcar', component: SearchContainer},
+    {path:'/search', component: ShopcarContainer}
+  ],
+  linkActiveClass: 'mui-active' // 选中的链接高亮显示
 })
 
 // 把路由对象暴露出去
